5220e12171be149a92dfbf0a554d10fc4c463a39
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-13 11:50:34 -0500

    learned currying

--- exercise4.js
+++ exercise4.js
@@ -11,18 +11,24 @@ const addTo = x => y => x + y
 var addToTen = addTo(10)
 addToTen(3) //This will return 13
 
+//Notes:
+//by itself addTo(10) will return y => x + y
+//But when you assigned it to a variable like addToTen
+//and then did addToTen(3). The 3 is inserted in place of y. 
+//Y returns x + y which is 10 + 3
+
 
 //Currying: What does the last line return?
 const sum = (a, b) => a + b
 const curriedSum = (a) => (b) => a + b
-curriedSum(30)(1)
+curriedSum(30)(1) //returns 31
 
 
 //Currying: What does the last line return?
 const sum = (a, b) => a + b
 const curriedSum = (a) => (b) => a + b
 const add5 = curriedSum(5)
-add5(12)
+add5(12) //returns 17
 
 //Composing: What does the last line return?
 const compose = (f, g) => (a) => f(g(a));
